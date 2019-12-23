86371d9787e8fe43c3b2565e4487516b32a06470
Katie Belanger <belangerkat@gmail.com>
2019-12-23 17:17:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,14 +17,15 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-  return "I can\'t hear you!";
-}
-if (string ==== string.toUpperCase()) {
-  return "YES INDEED!";
-}
-else {
-  return "I love you, too."
-}
-}
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
